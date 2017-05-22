7e04aafba6cf741c21aae8c88be98b8cf91bcd78
peterdyoon <peterdyoon@gmail.com>
2017-05-22 13:29:45 -0500

    added design

--- app.js
+++ app.js
@@ -18,11 +18,14 @@ app.controller('myController', function ($scope) {
     for (var i = 0; i < 10; i++) {
         myData.push({
             bowl1: 0,
+            bowl1display:'',
             strikebonus: 0,
             sparebonus: 0,
             bowl2: 0,
+            bowl2display:'',
             bowl2tab: false,
             bowl3: 0,
+            bowl3display:0,
             activetab: 1,
             bowl3tab: false,
             frametotal: 0,
@@ -38,11 +41,21 @@ app.controller('myController', function ($scope) {
         var dumArray = [];
         var myrange = 0
         if (frame.bowl1 === 10 && frame.bowl2 === 10) {
+            frame.bowl1display = 'X';
+            frame.bowl2display = 'X';
             return frame.bowl1Pad;
         } else if (frame.bowl1 === 10 && frame.activetab === 1) {
+            frame.bowl1display = 'X';
+            frame.bowl2display = frame.bowl2;
             return frame.bowl1Pad;
         } else if (frame.bowl1 === 10 && frame.activetab === 2) {
+            frame.bowl1display = 'X';
+            frame.bowl2display = frame.bowl2;
             myrange = 10 - frame.bowl2;
+        } else if (frame.bowl1 + frame.bowl2 === 10) {
+            frame.bowl1display = frame.bowl1;
+            frame.bowl2display = '/';
+            return frame.bowl1Pad;
         } else {
             myrange = 10 - frame.bowl1;
         }
@@ -60,11 +73,17 @@ app.controller('myController', function ($scope) {
         dumArray.push('/');
         return dumArray;
     }
-    $scope.calcTranslator = function(calc) {
+    $scope.calcTranslator = function(frame, calc) {
         if (typeof calc === 'string') {
             if (calc.toLowerCase() === 'x') {
                 return 10;
-            } 
+            } else if (calc === '/') {
+                if (frame.key === 9 && frame.activetab === 3) {
+                    return 10 - frame.bowl2;
+                } else {
+                    return 10 - frame.bowl1;
+                }
+            }
         } else {
             return calc;
         }
@@ -82,12 +101,17 @@ app.controller('myController', function ($scope) {
         showCheck(myData[frame.key]);
         for (var i = 0; i < myData.length; i++) {
             if (myData[i].bowl1 === 10) {
+                myData[i].bowl1display = 'X';
                 strikeCalculator(myData[i]);
                 myData[i].sparebonus = 0;
             } else if (myData[i].bowl2 + myData[i].bowl1 === 10 && myData[i].bowl1 !== 10) {
+                myData[i].bowl2display = '/';
                 spareCalculator(myData[i]);
                 myData[i].strikebonus = 0;
             } else {
+                myData[i].bowl1display = myData[i].bowl1;
+                myData[i].bowl2display = myData[i].bowl2;
+                myData[i].bowl3display = myData[i].bowl3;
                 myData[i].strikebonus = 0;
                 myData[i].sparebonus = 0;
             }
@@ -116,9 +140,13 @@ app.controller('myController', function ($scope) {
             }
         } else if (frame.key === 9) {
             if (myData[frame.key].bowl2 === 10) {
+                myData[frame.key].bowl2display = 'X';
                 myData[frame.key].strikebonus = 10 + myData[frame.key].bowl3;
             } else {
                 myData[frame.key].strikebonus = myData[frame.key].bowl2 + myData[frame.key].bowl3;
+            }
+            if (myData[frame.key].bowl3 === 10) {
+                myData[frame.key].bowl3display = 'X';
             }
         } else {
             if (myData[frame.key + 1].bowl1 === 10) {
@@ -129,6 +157,7 @@ app.controller('myController', function ($scope) {
         }
     }
     var spareCalculator = function (frame) {
+        myData[frame.key].sparebonus = 0;
         if (frame.key === 9) {
             myData[frame.key].sparebonus = myData[frame.key].bowl3;
         } else {
